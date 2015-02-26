1e677f9b643934564fd2b89000b8ffd5ff27cc03
paultgoodchild <paultgoodchild@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-26 14:39:14 +0000

    release plugin version 2.11.4
    git-svn-id: https://plugins.svn.wordpress.org/worpit-admin-dashboard-plugin/trunk@1100402 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/config_header.php
+++ views/config_header.php
@@ -1,6 +1,6 @@
 <?php
-
-include_once( dirname(__FILE__).ICWP_DS.'widgets'.ICWP_DS.'icwp_widgets.php' );
+$sBaseDirName = dirname(__FILE__).ICWP_DS;
+include_once( $sBaseDirName.'widgets'.ICWP_DS.'icwp_widgets.php' );
 
 function printOptionsPageHeader( $sSection = '', $sPluginName = ''  ) {
 	$sLinkedIcwp = '<a href="http://icwp.io/3a" target="_blank">iControlWP</a>';
@@ -220,7 +220,7 @@ function getPluginOptionSpan( $inaOption, $iSpanSize, $sVarPrefix = '' ) {
 	<div class="bootstrap-wpadmin <?php echo isset($icwp_sFeatureSlug) ? $icwp_sFeatureSlug : ''; ?>">
 		<div class="row">
 			<div class="span12">
-				<?php include_once( 'icwp-app-state_summary.php' ); ?>
+				<?php include_once( $sBaseDirName.'state_summary.php' ); ?>
 			</div>
 		</div>
-<?php echo printOptionsPageHeader( $icwp_sFeatureName, $icwp_sPluginName );
+<?php printOptionsPageHeader( $icwp_sFeatureName, $icwp_sPluginName );
